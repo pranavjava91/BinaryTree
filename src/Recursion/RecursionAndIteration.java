d600940f9eb0b82e724f3cc48a9a3ac1746de337
krishna <krishna@LAPTOP-VP3KSPPN>
2026-01-19 22:54:55 +0530

    recursion and iteration

---
+++ src/Recursion/RecursionAndIteration.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class RecursionAndIteration {
+	
+	 public static void main(String[] args) {
+		 callMyselfRecursion(9);
+		 callMyselfIteration(9);
+		 
+		    
+		  }
+		  /* The recursive Java method */
+		  public static void callMyselfRecursion(long i) {
+		    if (i < 0) {
+		      return;
+		    }
+		    System.out.print(i);
+		    i = i - 1;
+		    callMyselfRecursion(i);
+		  }
+		  public static void callMyselfIteration(long i) {
+			  System.out.println();
+			  for(int j=(int) i;j>=0;j--) {
+				  System.out.print(j);
+			  }
+			  
+		   }
+
+}
